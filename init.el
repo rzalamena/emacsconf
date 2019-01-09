cddb72fb95a65a9e44278705712b21f37f274bee
Rafael Zalamena <rzalamena@gmail.com>
2019-01-09 10:23:13 -0200

    Initial commit
    Import my new emacs configuration.

---
+++ init.el
@@ -0,0 +1,189 @@
+;;; init.el --- my personal configuration.
+
+;;; Commentary:
+;;
+;; Package handling.
+;;
+
+;;; Code:
+(package-initialize)
+
+;; Repository list.
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
+;; Packages to auto install.
+(defvar package-list '(
+		       ;; Auto complete.
+		       company
+		       company-c-headers
+		       company-web
+
+		       ;; Languages support.
+		       alchemist ;; Elixir.
+		       web-mode ;; template editing.
+		       gitignore-mode
+
+		       ;; Syntax checking.
+		       flycheck
+		       flycheck-clang-analyzer
+
+		       ;; Project handling.
+		       ggtags
+		       projectile
+
+		       ;; Git handling.
+		       magit
+
+		       ;; UI improvements.
+		       flx
+		       flx-ido
+		       helm
+		       ido-vertical-mode
+
+		       ;; Theme.
+		       monokai-theme
+		       ))
+
+;; Detect first time run to refresh package repository before installing
+;; required packages.
+(or (file-exists-p package-user-dir) (package-refresh-contents))
+
+;; Install if not installed.
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+
+;;
+;; GUI.
+;;
+(require 'monokai-theme) ;; theme.
+(scroll-bar-mode -1) ;; remove scroll bar.
+(menu-bar-mode -1) ;; remove menu bar.
+(tool-bar-mode -1) ;; remove tool bar.
+(setq inhibit-startup-screen t) ;; Don't show startup screen.
+(column-number-mode t) ;; show column.
+
+(set-frame-font "Hack:pixelsize=16")
+
+;; Save settings and buffers.
+(desktop-save-mode t)
+
+;; Improved navigation.
+(require 'flx-ido)
+(ido-mode 1)
+(ido-everywhere 1)
+(ido-vertical-mode t)
+(flx-ido-mode 1)
+
+(setq
+ ido-enable-flex-matching t
+ ;; disable ido faces to see flx highlights.
+ ido-use-faces nil
+ )
+
+;; Enable helm.
+(require 'helm-config)
+(global-set-key (kbd "M-x") 'helm-M-x)
+
+;; Make symbols pretty when possible.
+(global-prettify-symbols-mode t)
+
+
+;;
+;; Editing.
+;;
+(electric-pair-mode t) ;; automatically create pairs, say parentheses.
+(show-paren-mode t) ;; show matching parentheses.
+(setq make-backup-files nil) ;; disable backup files.
+(setq require-final-newline t) ;; add blank newline at the end files.
+
+;; Show annoying whitespaces.
+(require 'whitespace)
+(setq whitespace-style '(face trailing lines-tail empty
+			      space-after-tab::tab space-after-tab::space
+			      space-after-tab space-before-tab::tab
+			      space-before-tab::space space-before-tab))
+(global-whitespace-mode t)
+
+;; Spell checker.
+(with-eval-after-load "ispell"
+  (defvar ispell-program-name "hunspell")
+  (defvar ispell-dictionary "en_US,pt_BR")
+  (ispell-set-spellchecker-params)
+  (ispell-hunspell-add-multi-dic "en_US,pt_BR"))
+
+;; Auto complete.
+(require 'company)
+(global-company-mode t)
+(setq company-idle-delay 0) ;; Faster auto completion.
+
+(add-to-list 'company-backends
+	     '(alchemist-company
+	       company-c-headers
+	       company-gtags
+	       company-web-html
+	       company-web-jade
+	       company-web-slim))
+
+;; Syntax checking.
+(global-flycheck-mode t)
+
+;; Project handling.
+(projectile-mode t)
+(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
+;; OpenBSD C style plus tweaks.
+(defun c-newline ()
+  "C newline implementation.
+
+Check if we are inside a code comment to continue inside comment,
+otherwise just use the regular newline function."
+  (interactive)
+  (let ((langelem (caar (last (c-guess-basic-syntax)))))
+    (if (or (eq langelem 'comment-intro)
+	    (eq langelem 'c))
+	(c-indent-new-comment-line)
+      (newline-and-indent))))
+
+(defconst openbsd-c-style
+  '(
+    ;; Use tabs and represent them with 8 spaces.
+    (c-basic-offset . 8)
+    (c-tab-always-indent . t)
+
+    ;; Labels start at column 0 of the indentation.
+    (c-label-minimum-indentation . 0)
+
+    ;; When commenting with 'M-;' use the style(9) format.
+    (comment-style . extra-line)
+
+    ;; Some items to make code editing more pleasant.
+    (add-to-list 'c-cleanup-list
+		 '(brace-else-brace
+		   brace-elseif-brace
+		   defun-close-semi
+		   compact-empty-funcall)))
+  "OpenBSD C style.")
+(c-add-style "openbsd" openbsd-c-style)
+
+;; Automatically configure C buffers to use my preference.
+(add-hook 'c-mode-hook (lambda () (progn
+			       (ggtags-mode t)
+			       (c-set-style "openbsd")
+			       (define-key c-mode-map (kbd "RET") 'c-newline))))
+
+;; Always indent by default.
+(define-key global-map (kbd "RET") 'newline-and-indent)
+
+
+;;
+;; Emacs generated config:
+;; load it from your personal file and don't clutter the versioned files.
+;;
+(setq custom-file "~/.emacs.d/custom.el")
+(if (file-exists-p custom-file)
+    (load custom-file))
+
+(provide 'init)
